3fa2fe0f11ef67836a796b81f9194055dbd056a9
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-07-04 11:31:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,67 @@
-import bs4
+import os
+import telegram
+from telegram.ext import Updater, CommandHandler
 
-def convert_html_to_txt(html_file):
-    with open(html_file, "r") as f:
-        html = f.read()
+def convert_links_to_text(update, context):
+    # Get the file ID from the message
+    file_id = update.message.document.file_id
 
-    soup = bs4.BeautifulSoup(html, "html.parser")
+    # Get the file details using the file ID
+    file_details = context.bot.get_file(file_id)
 
-    links = []
-    for link in soup.findAll("a"):
-        name = link["name"]
-        href = link["href"]
-        links.append((name, href))
+    # Download the file
+    file_path = file_details.download()
 
-    with open("links.txt", "w") as f:
-        for name, href in links:
-            f.write(f"{name}: {href}\n")
+    # Create a new file to store the converted links
+    output_file_path = 'converted_links.txt'
 
-if __name__ == "__main__":
-    html_file = "my_html_file.html"
-    convert_html_to_txt(html_file)
+    # Convert links in the file
+    with open(file_path, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    converted_links = []
+    for line in lines:
+        line = line.strip()
+        if line.startswith('http') or line.startswith('www'):
+            name = line.split('/')[-1]
+            converted_links.append(f'{name}:{line}')
+
+    # Write the converted links to the output file
+    with open(output_file_path, 'w', encoding='utf-8') as output_file:
+        output_file.write('\n'.join(converted_links))
+
+    # Send the converted links file to the user
+    context.bot.send_document(
+        chat_id=update.effective_chat.id,
+        document=open(output_file_path, 'rb')
+    )
+
+    # Delete the downloaded and converted files
+    os.remove(file_path)
+    os.remove(output_file_path)
+
+def start(update, context):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Welcome! Please send me a text file to convert links to name:link format."
+    )
+
+def main():
+    # Replace 'YOUR_TOKEN' with your Telegram bot token
+    updater = Updater(token='YOUR_TOKEN', use_context=True)
+    dispatcher = updater.dispatcher
+
+    # Register the command handlers
+    start_handler = CommandHandler('start', start)
+    convert_links_handler = CommandHandler('convertlinks', convert_links_to_text)
+
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(convert_links_handler)
+
+    # Start the bot
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
 
