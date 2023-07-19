a962a38fa4db0a92b2ce90db5490e193e7c1c408
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-07-19 15:07:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Callb
 from bs4 import BeautifulSoup
 
 def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Hi! Please send me an HTML file or .html file from your storage.', reply_markup=ForceReply(selective=True))
+    update.message.reply_text('Hi! Please send me an HTML or .html file from your storage.', reply_markup=ForceReply(selective=True))
 
 def convert_html_to_txt(update: Update, context: CallbackContext) -> None:
     file = context.bot.getFile(update.message.document.file_id)
@@ -18,17 +18,33 @@ def convert_html_to_txt(update: Update, context: CallbackContext) -> None:
             for link in links:
                 name = link.text
                 url = link.get('href')
-                f.write(f'{name}:{url}\n')
+                f.write(f'{name}: {url}\n')
+    with open('output.txt', 'rb') as f:
+        update.message.reply_document(f)
+
+def combine_txt(update: Update, context: CallbackContext) -> None:
+    file = context.bot.getFile(update.message.document.file_id)
+    file.download('temp.txt')
+    with open('temp.txt', 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+        with open('output.txt', 'w', encoding='utf-8') as f:
+            for line in lines:
+                parts = line.split(':')
+                if len(parts) == 2:
+                    name = parts[0].strip()
+                    url = parts[1].strip()
+                    f.write(f'{name}:{url}\n')
     with open('output.txt', 'rb') as f:
         update.message.reply_document(f)
 
 def main() -> None:
-    updater = Updater("YOUR_TOKEN_HERE")
+    updater = Updater("6309773140:AAFaxUDW3IQ9fHa8jkUCcCT2-3oYV5wikso")
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(MessageHandler(Filters.document.file_extension("html"), convert_html_to_txt))
+    dispatcher.add_handler(MessageHandler(Filters.document.file_extension("txt"), combine_txt))
     updater.start_polling()
     updater.idle()
 
-if __name__ == '__main__':
+if name == 'main':
     main()
