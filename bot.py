c49fd56e52259a262d9cd168843fa5a473bd6ce4
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-07-19 16:06:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,51 +1,37 @@
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-from bs4 import BeautifulSoup
 import json
 
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Hi! Please send me an HTML, TXT or JSON file from your storage.', reply_markup=ForceReply(selective=True))
 
-def handle_document(update: Update, context: CallbackContext) -> None:
-    file = context.bot.getFile(update.message.document.file_id)
+def handle_file(update: Update, context: CallbackContext) -> None:
+    file = update.message.document.get_file()
     file_name = update.message.document.file_name
-    file_extension = file_name.split('.')[-1]
-    if file_extension not in ['html', 'txt', 'json']:
+    file_ext = file_name.split('.')[-1]
+    if file_ext not in ['html', 'txt', 'json']:
         update.message.reply_text('Invalid file type. Please send an HTML, TXT or JSON file.')
         return
     file_content = file.download_as_bytearray().decode('utf-8')
-    output = ''
-    if file_extension == 'html':
+    if file_ext == 'html':
+        from bs4 import BeautifulSoup
         soup = BeautifulSoup(file_content, 'html.parser')
-        for link in soup.find_all('a'):
-            name = link.text.strip()
-            url = link.get('href')
-            if name and url:
-                output += f'{name}: {url}\n'
-    elif file_extension == 'txt':
-        lines = file_content.split('\n')
-        for line in lines:
-            parts = line.split(':')
-            if len(parts) == 2:
-                name, url = parts
-                output += f'{name.strip()}: {url.strip()}\n'
-    elif file_extension == 'json':
+        links = soup.find_all('a')
+        result = '\n'.join([f'{link.text}:{link.get("href")}' for link in links])
+    elif file_ext == 'txt':
+        result = file_content
+    elif file_ext == 'json':
         data = json.loads(file_content)
-        for item in data:
-            if 'name' in item and 'url' in item:
-                output += f"{item['name']}: {item['url']}\n"
-    if output:
-        context.bot.send_document(chat_id=update.message.chat_id, filename='output.txt', document=output.encode('utf-8'), caption='Here is the converted TXT file.')
-    else:
-        update.message.reply_text('No valid name and URL pairs found in the file.')
+        result = '\n'.join([f'{k}:{v}' for k,v in data.items()])
+    update.message.reply_text(result)
 
 def main() -> None:
     updater = Updater("6309773140:AAFaxUDW3IQ9fHa8jkUCcCT2-3oYV5wikso")
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(Filters.document, handle_document))
+    dispatcher.add_handler(MessageHandler(Filters.document, handle_file))
     updater.start_polling()
     updater.idle()
 
-if name == 'main':
+if __name__ == '__main__':
     main()
