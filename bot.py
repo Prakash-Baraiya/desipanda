30cce56725aff4c2340f903a9f19134f80d7d806
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-07-19 13:14:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,37 @@
+import os
+import telebot
 from bs4 import BeautifulSoup
 
-# Open the HTML file
-with open("example.html", "r") as file:
-    html_content = file.read()
+bot = telebot.TeleBot("YOUR_BOT_TOKEN")
 
-# Create a BeautifulSoup object to parse the HTML content
-soup = BeautifulSoup(html_content, "html.parser")
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.send_message(message.chat.id, "Hi! I can convert HTML files to text files.")
+    bot.send_message(message.chat.id, "To convert an HTML file, send me the file.")
 
-# Find all the anchor tags in the HTML
-anchor_tags = soup.find_all("a")
+@bot.message_handler(content_types=["document"])
+def handle_document(message):
+    file_name = message.document.file_name
+    file_content = get_file_content(message.document.file_id)
 
-# Iterate through each anchor tag and extract the name and link
-result = ""
-for tag in anchor_tags:
-    href = tag["href"]
-    if href and href.startswith("https://"):
-        name = re.findall(r"(.+?)\.", href)[0]
-        result += f"{name}:{href}\n"
+    with open(file_name, "wb") as f:
+        f.write(file_content)
 
-# Save the extracted data in a text file
-with open("output.txt", "w") as file:
-    file.write(result)
+    text_content = convert_html_to_text(file_name)
+
+    bot.send_message(message.chat.id, f"Here is the converted text file: {file_name}: {text_content}")
+
+def convert_html_to_text(file_name):
+    text = ""
+    with open(file_name, "r") as f:
+        soup = BeautifulSoup(f, "html.parser")
+        for tag in soup.find_all("a"):
+            if tag.get("href") and tag.get("href").startswith("https://"):
+                name = re.findall(r"(.+?)\.", tag["href"])[0]
+                text += f"{name}:{tag['href']}\n"
+
+    return text
+
+if __name__ == "__main__":
+    bot.polling()
+                
