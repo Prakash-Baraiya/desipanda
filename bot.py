aa237850ab9dcbeea5dac53e85866ca403cf55f6
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-07-19 17:42:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,14 +3,14 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Callb
 import re
 
 def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Hi! Send me a text or .txt file and I will use a backspace character before replacing "https" with ":https".')
+    update.message.reply_text('Hi! Send me a text or .txt file and I will use two backspace characters before replacing "https" with ":https".')
 
 def handle_document(update: Update, context: CallbackContext) -> None:
     file = context.bot.getFile(update.message.document.file_id)
     file.download('temp.txt')
     with open('temp.txt', 'r') as f:
         text = f.read()
-    text = re.sub(r'https', r'\b:https', text)
+    text = re.sub(r'https', r'\b\b:https', text)
     with open('temp.txt', 'w') as f:
         f.write(text)
     update.message.reply_document(open('temp.txt', 'rb'))
