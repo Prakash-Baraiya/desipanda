20d6cb53fc6e05db332eb15d9cb6b09c5273ab56
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-07-19 15:36:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,12 +8,18 @@ def start(update: Update, context: CallbackContext) -> None:
 def convert_html_to_txt(update: Update, context: CallbackContext) -> None:
     file = context.bot.getFile(update.message.document.file_id)
     file.download('temp.html')
-    with open('temp.html', 'r') as f:
+    with open('temp.html', 'r', encoding='utf-8') as f:
         soup = BeautifulSoup(f.read(), 'html.parser')
         text = soup.get_text()
-        with open('temp.txt', 'w') as f:
+        links = soup.find_all('a')
+        with open('output.txt', 'w', encoding='utf-8') as f:
             f.write(text)
-    with open('temp.txt', 'rb') as f:
+            f.write('\n\nLinks:\n')
+            for link in links:
+                name = link.text
+                url = link.get('href')
+                f.write(f'{name}:{url}\n')
+    with open('output.txt', 'rb') as f:
         update.message.reply_document(f)
 
 def main() -> None:
