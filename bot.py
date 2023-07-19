c8c7ea059a3e852f4413b413b93997fcf802c3c6
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-07-19 13:45:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,16 +8,24 @@ def start(update: Update, context: CallbackContext) -> None:
 def convert_html_to_txt(update: Update, context: CallbackContext) -> None:
     file = context.bot.getFile(update.message.document.file_id)
     file.download('temp.html')
-    with open('temp.html', 'r') as f:
+    with open('temp.html', 'r', encoding='utf-8') as f:
         soup = BeautifulSoup(f.read(), 'html.parser')
         text = soup.get_text()
-        with open('temp.txt', 'w') as f:
+        with open('temp.txt', 'w', encoding='utf-8') as f:
             f.write(text)
+        links = soup.find_all('a')
+        with open('links.txt', 'w', encoding='utf-8') as f:
+            for link in links:
+                name = link.text
+                url = link.get('href')
+                f.write(f'{name}:{url}\n')
     with open('temp.txt', 'rb') as f:
+        update.message.reply_document(f)
+    with open('links.txt', 'rb') as f:
         update.message.reply_document(f)
 
 def main() -> None:
-    updater = Updater("6309773140:AAFaxUDW3IQ9fHa8jkUCcCT2-3oYV5wikso")
+    updater = Updater("YOUR_TOKEN_HERE")
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(MessageHandler(Filters.document.file_extension("html"), convert_html_to_txt))
