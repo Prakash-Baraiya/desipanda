d9d0da3c19cbf17496c92aa00bb3d0d1ba267051
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-07-19 15:11:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,27 +22,11 @@ def convert_html_to_txt(update: Update, context: CallbackContext) -> None:
     with open('output.txt', 'rb') as f:
         update.message.reply_document(f)
 
-def combine_txt(update: Update, context: CallbackContext) -> None:
-    file = context.bot.getFile(update.message.document.file_id)
-    file.download('temp.txt')
-    with open('temp.txt', 'r', encoding='utf-8') as f:
-        lines = f.readlines()
-        with open('output.txt', 'w', encoding='utf-8') as f:
-            for line in lines:
-                parts = line.split(':')
-                if len(parts) == 2:
-                    name = parts[0].strip()
-                    url = parts[1].strip()
-                    f.write(f'{name}:{url}\n')
-    with open('output.txt', 'rb') as f:
-        update.message.reply_document(f)
-
 def main() -> None:
     updater = Updater("6309773140:AAFaxUDW3IQ9fHa8jkUCcCT2-3oYV5wikso")
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(MessageHandler(Filters.document.file_extension("html"), convert_html_to_txt))
-    dispatcher.add_handler(MessageHandler(Filters.document.file_extension("txt"), combine_txt))
     updater.start_polling()
     updater.idle()
 
