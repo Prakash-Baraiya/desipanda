e08faf41d769a2c3ac7f950dc5e87889ba0cefbf
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-07-19 22:31:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -47,7 +47,7 @@ def format_text(text):
         elif i > 0 and re.match(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', lines[i-1]):
             continue
         else:
-            formatted_lines.append(line)
+            continue
     return '\n'.join(formatted_lines)
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
