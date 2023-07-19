ede1292b5a5db87993980664c054d05156e4fd94
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-07-19 16:57:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,44 +1,30 @@
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-from bs4 import BeautifulSoup
 
 def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Hi! Please send me an HTML file or .html file from your storage.', reply_markup=ForceReply(selective=True))
+    update.message.reply_text('Hi! Send me a text or .txt file and I will replace all "https" with ":https".')
 
-def convert_html_to_txt(update: Update, context: CallbackContext) -> None:
-    file = context.bot.getFile(update.message.document.file_id)
-    file.download('temp.html')
-    with open('temp.html', 'r') as f:
-        soup = BeautifulSoup(f.read(), 'html.parser')
-        text = soup.get_text()
-        with open('temp.txt', 'w') as f:
-            f.write(text)
-    with open('temp.txt', 'rb') as f:
-        update.message.reply_document(f)
-
-def process_txt_file(update: Update, context: CallbackContext) -> None:
+def handle_document(update: Update, context: CallbackContext) -> None:
     file = context.bot.getFile(update.message.document.file_id)
     file.download('temp.txt')
     with open('temp.txt', 'r') as f:
-        lines = f.readlines()
-        result = []
-        for line in lines:
-            parts = line.split()
-            if len(parts) >= 2:
-                name = parts[0]
-                url = parts[-1]
-                result.append(f'{name}: {url}')
-        with open('result.txt', 'w') as f:
-            f.write('\n'.join(result))
-    with open('result.txt', 'rb') as f:
-        update.message.reply_document(f)
+        text = f.read()
+    text = text.replace('https', ':https')
+    with open('temp.txt', 'w') as f:
+        f.write(text)
+    update.message.reply_document(open('temp.txt', 'rb'))
 
-def main() -> None:
+def handle_text(update: Update, context: CallbackContext) -> None:
+    text = update.message.text
+    text = text.replace('https', ':https')
+    update.message.reply_text(text)
+
+def main():
     updater = Updater("6309773140:AAFaxUDW3IQ9fHa8jkUCcCT2-3oYV5wikso")
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(Filters.document.file_extension("html"), convert_html_to_txt))
-    dispatcher.add_handler(MessageHandler(Filters.document.file_extension("txt"), process_txt_file))
+    dispatcher.add_handler(MessageHandler(Filters.document, handle_document))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_text))
     updater.start_polling()
     updater.idle()
 
