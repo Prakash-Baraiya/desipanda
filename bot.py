43efbfc0c8226b057f2b4585e23725cff2c46174
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-07-19 17:51:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,31 +1,49 @@
-from telegram import Update, ForceReply
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-import re
+from telegram import Update, Bot
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import os
 
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Hi! Send me a text or .txt file and I will use two backspace characters before replacing "https" with ":https".')
+TOKEN = '6309773140:AAFaxUDW3IQ9fHa8jkUCcCT2-3oYV5wikso'
+bot = Bot(TOKEN)
+updater = Updater(bot=bot)
 
-def handle_document(update: Update, context: CallbackContext) -> None:
+def start(update: Update, context):
+    update.message.reply_text('Hello! Send me a text or .txt file and I will extract all the links and names and format them as name:url.')
+
+def handle_text(update: Update, context):
+    text = update.message.text
+    formatted_text = format_text(text)
+    update.message.reply_text(formatted_text)
+
+def handle_document(update: Update, context):
     file = context.bot.getFile(update.message.document.file_id)
     file.download('temp.txt')
     with open('temp.txt', 'r') as f:
         text = f.read()
-    text = re.sub(r'https', r'\b\b:https', text)
-    with open('temp.txt', 'w') as f:
-        f.write(text)
-    update.message.reply_document(open('temp.txt', 'rb'))
+    formatted_text = format_text(text)
+    update.message.reply_text(formatted_text)
+    os.remove('temp.txt')
 
-def handle_text(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text("Please send me a text or .txt file.")
+def format_text(text):
+    lines = text.split('\n')
+    formatted_lines = []
+    for line in lines:
+        parts = line.split()
+        name = ''
+        url = ''
+        for part in parts:
+            if part.startswith('http'):
+                url = part
+            else:
+                name += part + ' '
+        name = name.strip()
+        if name and url:
+            formatted_line = f'{name}:{url}'
+            formatted_lines.append(formatted_line)
+    return '\n'.join(formatted_lines)
 
-def main():
-    updater = Updater("6309773140:AAFaxUDW3IQ9fHa8jkUCcCT2-3oYV5wikso")
-    dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(Filters.document, handle_document))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_text))
-    updater.start_polling()
-    updater.idle()
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, handle_text))
+updater.dispatcher.add_handler(MessageHandler(Filters.document, handle_document))
 
-if __name__ == '__main__':
-    main()
+updater.start_polling()
+updater.idle()
