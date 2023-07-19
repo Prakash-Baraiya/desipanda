b15807485888d319db4e99ecddb46b9e8e3bcf87
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-07-19 22:00:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,143 +14,49 @@ def start(update: Update, context):
 def handle_text(update: Update, context):
     text = update.message.text
     formatted_text = format_text(text)
-    send_formatted_text(formatted_text, context, update)
+    send_formatted_text(formatted_text, context)
 
 def handle_document(update: Update, context):
     file = context.bot.getFile(update.message.document.file_id)
-    file.download('temp')
-    with open('temp', 'r') as f:
-        text = f.read()
-    if update.message.document.mime_type == 'text/html':
-        formatted_text = format_html(text)
-    elif update.message.document.mime_type == 'application/json':
-        formatted_text = format_json(text)
-    else:
-        formatted_text = format_text(text)
-    send_formatted_text(formatted_text, context, update)
-    os.remove('temp')
+    file_name = update.message.document.file_name
+    file_ext = os.path.splitext(file_name)[1]
+    file.download(f'temp{file_ext}')
+    with open(f'temp{file_ext}', 'r') as f:
+        if file_ext == '.txt':
+            text = f.read()
+        elif file_ext == '.html':
+            text = f.read()
+        elif file_ext == '.json':
+            data = json.load(f)
+            text = json.dumps(data, indent=4)
+    formatted_text = format_text(text)
+    send_formatted_text(formatted_text, context)
+    os.remove(f'temp{file_ext}')
 
-def send_formatted_text(formatted_text, context, update):
-    if not formatted_text:
-        formatted_text = ''
+def send_formatted_text(formatted_text, context):
     with open('formatted_text.txt', 'w') as f:
         f.write(formatted_text)
     with open('formatted_text.txt', 'rb') as f:
         context.bot.send_document(chat_id=update.effective_chat.id, document=InputFile(f), filename='formatted_text.txt')
     os.remove('formatted_text.txt')
 
-def format_html(html):
-    from bs4 import BeautifulSoup
-    soup = BeautifulSoup(html, 'html.parser')
-    links = soup.find_all('a')
-    formatted_lines = []
-    for link in links:
-        name = link.text
-        url = link.get('href')
-        if url and re.match(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', url) and 'telegram.org' not in url:
-            formatted_line = f'{name}:{url}'
-            formatted_lines.append(formatted_line)
-    return '\n'.join(formatted_lines)
-
-def format_json(json_str):
-    data = json.loads(json_str)
-    formatted_lines = []
-    for key, value in data.items():
-        if isinstance(value, str) and re.match(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', value) and 'telegram.org' not in value:
-            formatted_line = f'{key}:{value}'
-            formatted_lines.append(formatted_line)
-    return '\n'.join(formatted_lines)
-
 def format_text(text):
     lines = text.split('\n')
     formatted_lines = []
-    
-    for i in range(len(lines)):
-        line=lines[i]
-        if re.match(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', line) and 'telegram.org' not in line:
-            name_line=lines[i-1]
-            formatted_line=f'{name_line}:{line}'
+    for i, line in enumerate(lines):
+        if re.match(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', line):
+            name_line = lines[i-1]
+            formatted_line = f'{name_line}:{line}'
             formatted_lines.append(formatted_line)
-            
-    
-            
-    
-            
-    
-            
-    
-            
-    
-            
-    
-            
-    
-            
-    
-            
-    
-            
-    
-            
-    
-            
-    
-            
-    
-            
-    
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-    
-    
-    
-    
-    
-    
+        else:
+            formatted_lines.append(line)
+    return '\n'.join(formatted_lines)
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(MessageHandler(Filters.text, handle_text))
-updater.dispatcher.add_handler(MessageHandler(Filters.document, handle_document))
+updater.dispatcher.add_handler(MessageHandler(Filters.document.file_extension('.txt'), handle_document))
+updater.dispatcher.add_handler(MessageHandler(Filters.document.file_extension('.html'), handle_document))
+updater.dispatcher.add_handler(MessageHandler(Filters.document.file_extension('.json'), handle_document))
 
 updater.start_polling()
 updater.idle()
