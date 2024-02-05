4e8040b6ffdc8a532b3206d02acc5863021d7de3
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2024-02-05 18:50:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,20 +16,29 @@ def handle_document(update: Update, context):
     file.download('temp.txt')
     with open('temp.txt', 'r') as f:
         text = f.read()
-    formatted_text = format_text(text)
+    
+    formatted_text, skipped_text = format_text(text)
+    
     with open('formatted_text.txt', 'w') as f:
         f.write(formatted_text)
-    with open('formatted_text.txt', 'a') as f:
-        remaining_lines = [line for line in text.split('\n') if line not in formatted_text.split('\n')]
-        f.write('\n'.join(remaining_lines))
     with open('formatted_text.txt', 'rb') as f:
         context.bot.send_document(chat_id=update.effective_chat.id, document=InputFile(f), filename='formatted_text.txt')
+    
+    if skipped_text:
+        with open('skipped_text.txt', 'w') as f:
+            f.write(skipped_text)
+        with open('skipped_text.txt', 'rb') as f:
+            context.bot.send_document(chat_id=update.effective_chat.id, document=InputFile(f), filename='skipped_text.txt')
+
     os.remove('temp.txt')
     os.remove('formatted_text.txt')
+    os.remove('skipped_text.txt')
 
 def format_text(text):
     lines = text.split('\n')
     formatted_lines = []
+    skipped_lines = []
+    current_name = ""
     
     # Lines already in "name:https" format will be preserved in the final output
     existing_lines = [line for line in lines if re.match(r'.+:https?://[^\s]+', line)]
@@ -39,12 +48,19 @@ def format_text(text):
         url_parts = urlparse(line.strip())
         if url_parts.scheme and url_parts.netloc:  # Check if it's a valid URL
             url = line.strip()
-            name = line.split(' ', 1)[0] if ':' not in line else line.split(':', 1)[0]
+            name = current_name if current_name and ':' not in current_name else 'no name'
             formatted_line = f'{name}:{url}'
             if formatted_line not in formatted_lines:  # Ensure unique pairs
                 formatted_lines.append(formatted_line)
+                current_name = ""  # Reset current_name for the next URL
+        else:
+            current_name += line.strip()  # Extend the current name until a URL is encountered
+            skipped_lines.append(line.strip())
 
-    return '\n'.join(formatted_lines)
+    formatted_text = '\n'.join(formatted_lines)
+    skipped_text = '\n'.join(skipped_lines)
+
+    return formatted_text, skipped_text
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(MessageHandler(Filters.document, handle_document))
