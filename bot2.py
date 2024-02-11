417420e6a4bc3c56fe6e6a54b7cd8e471a4c8509
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2024-02-12 00:39:24 +0530

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
-import re  # Add this import statement
+import re
 
 # Replace 'YOUR_API_ID' and 'YOUR_API_HASH' with your actual values
 API_ID = "11657097"
@@ -38,4 +38,3 @@ def format_output(urls_and_names: list) -> str:
 
 if __name__ == "__main__":
     app.run()
-    
